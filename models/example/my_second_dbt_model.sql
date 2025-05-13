bd02656948780579672f913ecbba964343fb71e2
Ezhil Anand <ezhil.nz@gmail.com>
2025-05-13 10:13:36 +0000

    Initial commit

---
+++ models/example/my_second_dbt_model.sql
@@ -0,0 +1,14 @@
+
+-- Use the `ref` function to select from other models
+
+select id
+from {{ ref('my_first_dbt_model') }}
+
+union all
+select id+1 from {{ ref('my_first_dbt_model')}}
+
+union all
+select id+2 from {{ ref('my_first_dbt_model')}}
+
+union all
+select id+3 from {{ ref('my_first_dbt_model')}}
