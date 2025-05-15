b14d02db775b7e7ba578be6f5decc19dd13c67bd
ezi <ezhil.nz@gmail.com>
2025-05-16 09:55:22 +1000

    updated

--- models/star/my_third_dbt_model.sql
+++ models/star/my_third_dbt_model.sql
@@ -1,8 +1,1 @@
-
--- Use the `ref` function to select from other models
-
-select id
-from {{ ref('my_first_dbt_model') }}
-
-union all
-select id+1 from {{ ref('my_second_dbt_model')}}
+select * from patents-public-data.cpc.definition
